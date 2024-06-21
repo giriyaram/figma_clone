1a40b14f38f2878b0f885ab1dd5b1aa1f1cb3ccf
Giri Yaram <yaramgiri@gmail.com>
2024-06-21 13:24:22 +0530

    initial commit

---
+++ app/Room.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_z-DIR_n3uat0_WFtpHWDVw3Xhb-FPwDc2eBpxQA6TihSdspZN5lIGpD1Gk8sWXKw"}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Page() {
+  return (
+      <h1 className="text-5xl text-white">Live Blocks Figma CLone</h1>
+    
+  );
+}
